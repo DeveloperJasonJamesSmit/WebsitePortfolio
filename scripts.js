9a5715a089862bc992412c66fb25cbd8a90b6364
DeveloperJasonJamesSmit <jason.developer@outlook.com>
2024-01-25 18:57:34 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    scrollfunction();
+}
+
+function scrollfunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
